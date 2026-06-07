1086a1179f79f3ffee3cfd6aac0f89d43e0b8a10
sunnydaddy <xunshoushi@126.com>
2026-06-07 18:55:26 +0800

    Initial Commit

---
+++ HoradricCube/HoradricCubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoradricCubeApp.swift
+//  HoradricCube
+//
+//  Created by Cici on 2026/6/7.
+//
+
+import SwiftUI
+
+@main
+struct HoradricCubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
